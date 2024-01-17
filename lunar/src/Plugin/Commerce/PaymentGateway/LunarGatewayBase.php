17b6e7b974cfe714db0d330fe68c56ee603f3232
lucionescu <lucian.ionesq@gmail.com>
2024-01-17 19:12:22 +0200

    Renamed base class & form class

--- lunar/src/Plugin/Commerce/PaymentGateway/LunarGatewayBase.php
+++ lunar/src/Plugin/Commerce/PaymentGateway/LunarGatewayBase.php
@@ -3,7 +3,6 @@
 namespace Drupal\commerce_lunar\Plugin\Commerce\PaymentGateway;
 
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
-use Drupal\commerce_order\Entity\Order;
 use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_payment\Entity\PaymentInterface;
 use Drupal\commerce_payment\Entity\PaymentMethodInterface;
@@ -19,24 +18,17 @@ use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * Provides the Off-site payment gateway.
- *
- * @CommercePaymentGateway(
- *   id = "lunar",
- *   label = "Lunar Card",
- *   display_label = "Card",
- *   forms = {
- *     "offsite-payment" = "\Drupal\commerce_lunar\PluginForm\PaymentLunarForm",
- *   },
- * )
+ * 
  */
-class Lunar extends OffsitePaymentGatewayBase implements LunarInterface
+abstract class LunarGatewayBase extends OffsitePaymentGatewayBase implements LunarInterface
 {
-  protected $paymentMethodCode = 'card';
-  private $apiClient;
+  const INTENT_ID_KEY = '_lunar_intent_id';
 
-  private $testMode = false;
-  private $isMobilePay = false;
+  protected $paymentMethodCode = '';
+  protected $apiClient;
+
+  protected $testMode = false;
+  protected $isMobilePay = false;
 
   /**
    * {@inheritdoc}
@@ -63,8 +55,8 @@ class Lunar extends OffsitePaymentGatewayBase implements LunarInterface
   public function defaultConfiguration()
   {
     return [
-      'transaction_type' => 'delayed',
-      'description' => 'Secure payment with '.($this->isMobilePay ? 'MobilePay' : 'card'). ' via © Lunar',
+      'capture_mode' => 'delayed',
+      'description' => 'Secure payment with ' . ($this->isMobilePay ? 'MobilePay' : 'card') . ' via © Lunar',
       'shop_title' => \Drupal::config('system.site')->get('name'),
     ] + parent::defaultConfiguration();
   }
@@ -91,12 +83,6 @@ class Lunar extends OffsitePaymentGatewayBase implements LunarInterface
         '#default_value' => $this->configuration['public_key'],
         '#required' => TRUE,
       ],
-      'logo_url' => [
-        '#type' => 'textfield',
-        '#title' => $this->t('Logo URL'),
-        '#default_value' => $this->configuration['logo_url'],
-        '#required' => TRUE,
-      ]
     ];
 
     if ($this->isMobilePay) {
@@ -111,14 +97,20 @@ class Lunar extends OffsitePaymentGatewayBase implements LunarInterface
     }
 
     $formFields = array_merge($formFields, [
-      'transaction_type' => [
+      'logo_url' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Logo URL'),
+        '#default_value' => $this->configuration['logo_url'],
+        '#required' => TRUE,
+      ],
+      'capture_mode' => [
         '#type' => 'radios',
-        '#title' => $this->t('Transaction type'),
+        '#title' => $this->t('Capture mode'),
         '#options' => [
           'delayed' => $this->t('Delayed'),
           'instant' => $this->t('Instant'),
         ],
-        '#default_value' => $this->configuration['transaction_type'],
+        '#default_value' => $this->configuration['capture_mode'],
         '#description' => $this->t('If you deliver your product instantly (e.g. a digital product), choose Instant mode. If not, use Delayed.'),
       ],
       'description' => [
@@ -151,12 +143,10 @@ class Lunar extends OffsitePaymentGatewayBase implements LunarInterface
       $this->configuration['app_key'] = $values['app_key'];
       $this->configuration['public_key'] = $values['public_key'];
       $this->configuration['logo_url'] = $values['logo_url'];
-      $this->configuration['transaction_type'] = $values['transaction_type'];
+      $this->configuration['capture_mode'] = $values['capture_mode'];
       $this->configuration['description'] = $values['description'];
       $this->configuration['shop_title'] = $values['shop_title'];
 
-      $this->configuration['mode'] = $this->defaultConfiguration()['mode'];
-      
       if ($this->isMobilePay) {
         $this->configuration['configuration_id'] = $values['configuration_id'];
       }
@@ -166,148 +156,65 @@ class Lunar extends OffsitePaymentGatewayBase implements LunarInterface
   /**
    * {@inheritdoc}
    */
-  public function createPayment(PaymentInterface $payment, $capture = TRUE)
+  public function onReturn(OrderInterface $order, Request $request)
   {
-    $this->assertPaymentState($payment, ['new']);
-    $payment_method = $payment->getPaymentMethod();
-    assert($payment_method instanceof PaymentMethodInterface);
-    $this->assertPaymentMethod($payment_method);
-    $order = $payment->getOrder();
-    assert($order instanceof OrderInterface);
+    $logger = \Drupal::logger('commerce_lunar');
 
-    $amount = $payment->getAmount();
-    $remote_id = $payment_method->getRemoteId();
-    $payment->setState('authorization');
-    $payment->setRemoteId($remote_id);
+    // This creates a payment method as a
+    // part of processing the payment data returned from the gateway.
+    $payment_method_storage = $this->entityTypeManager->getStorage('commerce_payment_method');
+    assert($payment_method_storage instanceof PaymentMethodStorageInterface);
+    $payment_method = $payment_method_storage->createForCustomer(
+      'credit_card',
+      $this->parentEntity->id(),
+      $order->getCustomerId(),
+      $order->getBillingProfile()
+    );
+
+    $payment_intent_id = $order->data->{self::INTENT_ID_KEY};
+
+    // The payment method is created first so that it can be attached to the
+    // generated payment transaction.
+    $this->createPaymentMethod($payment_method, $payment_intent_id);
+
+    $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
+    /** @var PaymentInterface $payment */
+    $payment = $payment_storage->create([
+      'state' => 'authorization',
+      'amount' => $order->getBalance(),
+      'payment_gateway' => $this->parentEntity->id(),
+      'payment_gateway_mode' => $this->testMode ? 'test' : null,
+      'order_id' => $order->id(),
+      'remote_id' => $payment_intent_id,
+      'payment_method' => $payment_method,
+      'remote_state' => $request->query->get('payment_status'),
+    ]);
+
+    $logger->info('Saving Payment information. Transaction reference: ' . $payment_intent_id);
+
     $payment->save();
 
-    if ($capture) {
-      $this->capturePayment($payment, $amount);
+    drupal_set_message('Payment was processed');
+
+    if ('instant' === $this->configuration['capture_mode']) {
+      $this->capturePayment($payment);
     }
+
+    $logger->info('Payment information saved successfully. Transaction reference: ' . $payment_intent_id);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function capturePayment(PaymentInterface $payment, Price $amount = NULL)
+  public function createPaymentMethod(PaymentMethodInterface $payment_method, $transaction_id)
   {
-    $this->assertPaymentState($payment, ['authorization']);
-    // If not specified, capture the entire amount.
-    $amount = (string) $amount ?: $payment->getAmount();
-
-    $remote_id = $payment->getRemoteId();
-    try {
-      $apiResponse = $this->apiClient->payments()->capture($remote_id, [
-        'amount' => [
-          'currency' => $payment->getOrder()->getTotalPrice()->getCurrencyCode(),
-          'decimal' => $amount,
-        ]
-      ]);
-
-      if ('completed' != $apiResponse['captureState']) {
-        throw new PaymentGatewayException($apiResponse['declinedReason']['error'] ?? 'Capture failed');
-      }
-
-      $payment->setState('completed');
-      $payment->setAmount($amount);
-      $payment->save();
-
-    } catch (\Lunar\Exception\ApiException $e) {
-      \Drupal::logger('commerce_lunar')->warning($e->getMessage());
-      throw new PaymentGatewayException($this->t('Capture failed. Transaction @id. @message', ['@id' => $remote_id, '@message' => $e->getMessage()]));
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function voidPayment(PaymentInterface $payment)
-  {
-    $this->assertPaymentState($payment, ['authorization']);
-    $remote_id = $payment->getRemoteId();
-    $amount = (string) $payment->getAmount();
-    try {
-      $apiResponse = $this->apiClient->payments()->cancel($remote_id,  [
-        'amount' => [
-          'currency' => $payment->getOrder()->getTotalPrice()->getCurrencyCode(),
-          'decimal' => $amount,
-        ]
-      ]);
-
-      if ('completed' != $apiResponse['cancelState']) {
-        throw new PaymentGatewayException($apiResponse['declinedReason']['error'] ?? 'Cancel failed');
-      }
-
-      $payment->setState('authorization_voided');
-      $payment->save();
-
-    } catch (\Lunar\Exception\ApiException $e) {
-      \Drupal::logger('commerce_lunar')->warning($e->getMessage());
-      throw new PaymentGatewayException($this->t('Capture failed. Transaction @id. @message', ['@id' => $remote_id, '@message' => $e->getMessage()]));
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function refundPayment(PaymentInterface $payment, Price $amount = NULL)
-  {
-    $this->assertPaymentState($payment, ['completed', 'partially_refunded']);
-    // If not specified, refund the entire amount.
-    $amount = (string) $amount ?: $payment->getAmount();
-    $this->assertRefundAmount($payment, $amount);
-
-    $remote_id = $payment->getRemoteId();
-    try {
-      $apiResponse = $this->apiClient->payments()->refund($remote_id,  [
-        'amount' => [
-          'currency' => $payment->getOrder()->getTotalPrice()->getCurrencyCode(),
-          'decimal' => $amount,
-        ]
-      ]);
-
-      if ('completed' != $apiResponse['refundState']) {
-        throw new PaymentGatewayException($apiResponse['declinedReason']['error'] ?? 'Refund failed');
-      }
-      
-      $old_refunded_amount = $payment->getRefundedAmount();
-      $new_refunded_amount = $old_refunded_amount->add($amount);
-      if ($new_refunded_amount->lessThan($payment->getAmount())) {
-        $payment->setState('partially_refunded');
-      } else {
-        $payment->setState('refunded');
-      }
-      $payment->setRefundedAmount($new_refunded_amount);
-      $payment->save();
-
-    } catch (\Lunar\Exception\ApiException $e) {
-      \Drupal::logger('commerce_lunar')->warning($e->getMessage());
-      throw new PaymentGatewayException($this->t('Refund failed. Transaction @id. @message', ['@id' => $remote_id, '@message' => $e->getMessage()]));
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function createPaymentMethod(PaymentMethodInterface $payment_method, array $payment_details)
-  {
-    // create payment intent ?
-
-    $transaction = $this->getTransaction($payment_details['lunar_transaction_id']);
-    if (isset($transaction['payment'])) {
-      $payment_method->setRemoteId($transaction['id']);
+    $transaction = $this->getTransaction($transaction_id);
+    if (isset($transaction['authorisationCreated']) && $transaction['authorisationCreated'] === true) {
+      $payment_method->setRemoteId($transaction_id);
       $payment_method->save();
     } else {
       throw new PaymentGatewayException($this->t('Transaction failed. Transaction @id.', ['@id' => $transaction['id']]));
     }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function deletePaymentMethod(PaymentMethodInterface $payment_method)
-  {
-    $payment_method->delete();
   }
 
   /**
@@ -322,40 +229,109 @@ class Lunar extends OffsitePaymentGatewayBase implements LunarInterface
       throw new InvalidRequestException($this->t('Transaction @id not found. @message', ['@id' => $transactionId, '@message' => $e->getMessage()]));
     }
   }
-  
+
   /**
-   * @return array
+   * {@inheritdoc}
    */
-  protected function getAddressInfo(Order $order) {
-    $entity_manager = \Drupal::entityTypeManager();
-    $billingProfile = $order->getBillingProfile();
+  public function capturePayment(PaymentInterface $payment, Price $amount = NULL)
+  {
+    $this->assertPaymentState($payment, ['authorization']);
+    // If not specified, capture the entire amount.
+    $amount = $amount ?: $payment->getAmount();
 
-    if (!$billingProfile) {
-      $customer = $order->getCustomer();
-      $billingProfile = $entity_manager->getStorage('profile')->loadDefaultByUser($customer, 'customer');
+    $payment_intent_id = $payment->getRemoteId();
+    try {
+      $apiResponse = $this->apiClient->payments()->capture($payment_intent_id, [
+        'amount' => [
+          'currency' => $amount->getCurrencyCode(),
+          'decimal' => (string) $amount->getNumber(),
+        ]
+      ]);
+
+      if ('completed' != $apiResponse['captureState']) {
+        throw new PaymentGatewayException($apiResponse['declinedReason']['error'] ?? 'Capture failed');
+      }
+
+      $payment->setState('completed');
+      $payment->setAmount($amount);
+      $payment->save();
+    } catch (\Lunar\Exception\ApiException $e) {
+      \Drupal::logger('commerce_lunar')->warning($e->getMessage());
+      throw new PaymentGatewayException($this->t('Capture failed. Transaction @id. @message', ['@id' => $payment_intent_id, '@message' => $e->getMessage()]));
     }
+  }
 
-    $data = [
-      'address' => '',
-      'name' => '',
-    ];
+  /**
+   * {@inheritdoc}
+   */
+  public function voidPayment(PaymentInterface $payment)
+  {
+    $this->assertPaymentState($payment, ['authorization']);
+    $payment_intent_id = $payment->getRemoteId();
+    $amount = $payment->getAmount();
+    try {
+      $apiResponse = $this->apiClient->payments()->cancel($payment_intent_id,  [
+        'amount' => [
+          'currency' => $amount->getCurrencyCode(),
+          'decimal' => (string) $amount->getNumber(),
+        ]
+      ]);
 
-    if ($billingProfile) {
-      $addressInfo = array($billingProfile->get('address')->first());
-      $data['address'] = implode(', ', array_filter([
-        $addressInfo['postal_code'],
-        $addressInfo['country_code'],
-        $addressInfo['administrative_area'],
-        $addressInfo['locality'],
-        $addressInfo['address_line1'],
-        $addressInfo['address_line2'],
-      ]));
-      $data['name'] = implode(' ', array_filter([
-        $addressInfo['family_name'],
-        $addressInfo['given_name'],
-      ]));
+      if ('completed' != $apiResponse['cancelState']) {
+        throw new PaymentGatewayException($apiResponse['declinedReason']['error'] ?? 'Cancel failed');
+      }
+
+      $payment->setState('authorization_voided');
+      $payment->save();
+    } catch (\Lunar\Exception\ApiException $e) {
+      \Drupal::logger('commerce_lunar')->warning($e->getMessage());
+      throw new PaymentGatewayException($this->t('Capture failed. Transaction @id. @message', ['@id' => $payment_intent_id, '@message' => $e->getMessage()]));
     }
+  }
 
-    return $data;
+  /**
+   * {@inheritdoc}
+   */
+  public function refundPayment(PaymentInterface $payment, Price $amount = NULL)
+  {
+    $this->assertPaymentState($payment, ['completed', 'partially_refunded']);
+    // If not specified, refund the entire amount.
+    $amount = $amount ?: $payment->getAmount();
+    $this->assertRefundAmount($payment, $amount);
+
+    $payment_intent_id = $payment->getRemoteId();
+    try {
+      $apiResponse = $this->apiClient->payments()->refund($payment_intent_id,  [
+        'amount' => [
+          'currency' => $amount->getCurrencyCode(),
+          'decimal' => (string) $amount->getNumber(),
+        ]
+      ]);
+
+      if ('completed' != $apiResponse['refundState']) {
+        throw new PaymentGatewayException($apiResponse['declinedReason']['error'] ?? 'Refund failed');
+      }
+
+      $old_refunded_amount = $payment->getRefundedAmount();
+      $new_refunded_amount = $old_refunded_amount->add($amount);
+      if ($new_refunded_amount->lessThan($payment->getAmount())) {
+        $payment->setState('partially_refunded');
+      } else {
+        $payment->setState('refunded');
+      }
+      $payment->setRefundedAmount($new_refunded_amount);
+      $payment->save();
+    } catch (\Lunar\Exception\ApiException $e) {
+      \Drupal::logger('commerce_lunar')->warning($e->getMessage());
+      throw new PaymentGatewayException($this->t('Refund failed. Transaction @id. @message', ['@id' => $payment_intent_id, '@message' => $e->getMessage()]));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deletePaymentMethod(PaymentMethodInterface $payment_method)
+  {
+    $payment_method->delete();
   }
 }
